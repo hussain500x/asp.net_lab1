cfe7ea60467849d81956aaf56ae5be0492bad52d
hussain500x <82269242+hussain500x@users.noreply.github.com>
2023-08-17 11:09:38 +0300

    first commit

---
+++ WebApplication1/Models/Record.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string RecordName { get; set; }
+        public string RecordDetail { get; set; }
+    }
+}
